676092a8359be63a19f3e0ebab9ea2f3cdcd0783
Dtikoli <tikolidavid@yahoo.com>
2023-12-14 06:39:33 -0800

    Adding alternative

--- 0x00-MySQL_Advanced/101-average_weighted_score_alt.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score_alt.sql
@@ -5,13 +5,11 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-  UPDATE users 
-  JOIN (
-    SELECT users.id, SUM(score * weight) / SUM(weight) AS w_avg FROM users 
+  UPDATE users, 
+    (SELECT users.id, SUM(score * weight) / SUM(weight) AS w_avg FROM users 
     JOIN corrections ON users.id = corrections.user_id 
     JOIN projects ON corrections.project_id = projects.id 
-    GROUP BY users.id
-  ) AS users_avgs
+    GROUP BY users.id) AS users_avgs
   SET users.average_score = users_avgs.w_avg 
   WHERE users.id = users_avgs.id;
 END;
