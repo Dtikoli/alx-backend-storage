e82c6c3a25b73c208658fc51e0b7322d25878252
Dtikoli <tikolidavid@yahoo.com>
2023-12-20 05:03:11 -0800

    Updating solution

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ A module with tools for request caching and tracking. """
-import redis
 import requests
-from datetime import timedelta
+import redis
 
 
 def get_page(url: str) -> str:
@@ -10,15 +9,20 @@ def get_page(url: str) -> str:
     Returns the content of a URL after caching the request's response,
     and tracking the request.
     """
-    if url is None or len(url.strip()) == 0:
-        return ""
-    redis_store = redis.Redis()
-    res_key = 'result:{}'.format(url)
-    req_key = 'count:{}'.format(url)
-    result = redis_store.get(res_key)
-    if result is not None:
-        redis_store.incr(req_key)
-        return result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), result)
-    return result
+    r = redis.Redis()
+    count_key = f"count:{url}"
+    count = r.get(count_key)
+    if count is None:
+        count = 0
+    else:
+        count = int(count)
+    count += 1
+    r.set(count_key, count, ex=10)
+    cache_key = f"cached:{url}"
+    cached_content = r.get(cache_key)
+    if cached_content:
+        return cached_content.decode("utf-8")
+    response = requests.get(url)
+    html_content = response.text
+    r.set(cache_key, html_content, ex=10)
+    return html_content
